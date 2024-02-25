6578f29e4d4f2c6ab2e3251d69f979626632b1a9
Shreya Srivastava <112487189+shreyasrivastava11@users.noreply.github.com>
2024-02-25 23:12:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,52 +8,61 @@
 
 for(let i = 0;i<document.querySelectorAll(".drum").length;i++)
 {
-    document.querySelectorAll(".drum")[i].addEventListener("click",function() {
-
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
         let buttoninnerHTML = this.innerHTML;
-
-        switch(buttoninnerHTML) {
-            case "w":
-                let audio = new Audio("sounds/tom-1.mp3");
-                audio.play();
-            break;
-
-            case "a":
-                let audio1 = new Audio("sounds/tom-2.mp3");
-                audio1.play();
-            break;
-
-            case "s":
-                let audio2 = new Audio("sounds/tom-3.mp3");
-                audio2.play();
-            break;
-
-            case "d":
-                let audio3 = new Audio("sounds/tom-4.mp3");
-                audio3.play();
-            break;
-
-            case "j":
-                let audio4 = new Audio("sounds/crash.mp3");
-                audio4.play();
-            break;
-
-            case "k":
-                let audio5 = new Audio("sounds/kick-bass.mp3");
-                audio5.play();
-            break;
-
-            case "l":
-                let audio6 = new Audio("sounds/snare.mp3");
-                audio6.play();
-            break;
-
-            default:
-                console.log(buttoninnerHTML);
-        }
-        // this.style.color = "white";
-
-        // "this" will specify the clicking button and the clicking button changes its original color to white thorugh this
-        //This happened when click on the button takes place.
+        soundPlay(buttoninnerHTML);
     });
+}
+
+document.addEventListener("keypress",function(event){
+    soundPlay(event.key);
+});
+
+function soundPlay(key){
+
+
+    switch(key) {
+        case "w":
+            let audio = new Audio("sounds/tom-1.mp3");
+            // Here 'Audio' is the name of the constructor function with file location and having the content of play as function.
+            audio.play();
+        break;
+
+        case "a":
+            let audio1 = new Audio("sounds/tom-2.mp3");
+            audio1.play();
+        break;
+
+        case "s":
+            let audio2 = new Audio("sounds/tom-3.mp3");
+            audio2.play();
+        break;
+
+        case "d":
+            let audio3 = new Audio("sounds/tom-4.mp3");
+            audio3.play();
+        break;
+
+        case "j":
+            let audio4 = new Audio("sounds/crash.mp3");
+            audio4.play();
+        break;
+
+        case "k":
+            let audio5 = new Audio("sounds/kick-bass.mp3");
+            audio5.play();
+        break;
+
+        case "l":
+            let audio6 = new Audio("sounds/snare.mp3");
+            audio6.play();
+        break;
+
+        default:
+            console.log(buttoninnerHTML);
+    }
+    // this.style.color = "white";
+
+    // "this" will specify the clicking button and the clicking button changes its original color to white thorugh this
+    //This happened when click on the button takes place.
 }
